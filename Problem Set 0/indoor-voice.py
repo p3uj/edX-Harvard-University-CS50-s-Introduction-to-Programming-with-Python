adcb51a8fc5a5789f9e74d3cb6e1c2f8d1f13ada
Bengie Villesco <bvillesco@gmail.com>
2023-08-16 17:07:44 +0800

    Creating Problem Set 0 folder and indoor-voice.py

---
+++ Problem Set 0/indoor-voice.py
@@ -0,0 +1,13 @@
+# Get the user's input and convert into lowercase and remove whitespace from the str
+text = input().lower().strip()
+
+"""
+Here is the code when we want to break down on
+how to convert the user's input into lowercase
+and remove the whitespace from the str
+
+text = text.lower().strip()
+"""
+
+# Display user's input
+print(text)
