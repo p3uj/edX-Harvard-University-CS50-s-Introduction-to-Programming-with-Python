ed02e025ac939fb20e41d9768d9afddb3bfd3083
Bengie Villesco <bvillesco@gmail.com>
2023-08-16 17:21:02 +0800

    Creating playback.py

---
+++ Problem Set 0/playback.py
@@ -0,0 +1,12 @@
+# Get the user's input and repplace the space into three periods.
+userInput = input().replace(" ", "...")
+
+"""
+Here is the code when we want to break down on
+how we can replace the space into three periods.
+
+userInput = userInput.replace(" ", "...")
+"""
+
+# Display user's input replacing each space with 3 periods.
+print(userInput)
