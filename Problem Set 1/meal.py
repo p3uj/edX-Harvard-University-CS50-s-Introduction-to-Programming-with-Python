8bf96ff018418427e7ef7ec8d6131fe240b099ef
Bengie Villesco <bvillesco@gmail.com>
2023-08-23 12:58:31 +0800

    Modified: meal.py (am/pm to a.m./p.m.)

--- Problem Set 1/meal.py
+++ Problem Set 1/meal.py
@@ -9,14 +9,13 @@ def main():
 
 def convert(time):
     hours, minutes = time.split(":")    # Separate the value for time and minutes
-    if minutes[-2:].lower() == "am" or minutes[-2:].lower() == "pm":
-        minutes, status = minutes.split(" ")
+    if minutes[-4:].lower() == "a.m." or minutes[-4:].lower() == "p.m.":    # Execute this if statement if the last four characters of minutes are "a.m." or "p.m.".
+        minutes, status = minutes.split(" ")    # Separate the value for minutes (numbers) and value for status (am or pm) withe the <space> as the separation cue.
     if minutes == "00":
          result = float(hours) + float(minutes)
     else:
         result = 1.0 / (60/float(minutes)) + float(hours)   # Convert the value of hours and minutes to float then compute
     return result
-
 
 if __name__ == "__main__":
     main()
