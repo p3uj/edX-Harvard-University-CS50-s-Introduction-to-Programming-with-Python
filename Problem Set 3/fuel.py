d7514c911934cb4b87b659542f8fd6df954d91ca
Bengie Villesco <bvillesco@gmail.com>
2023-08-27 16:13:32 +0800

    Create Problem Set 3 folder and fuel.py inside it

---
+++ Problem Set 3/fuel.py
@@ -0,0 +1,23 @@
+def main():
+    result = compute()  # Function call to compute() function.
+    if result <= 1:
+        print("E")
+    elif result == 99 or result == 100:
+        print("F")
+    elif result > 101:
+        compute()
+    else:
+        print(f"{result}%")
+
+
+def compute():
+    while True:
+        try:
+            x, y = (input("Fraction: ")).split("/") # Get the user input and separate that for x and y. "/" is a separation cue.
+            result = int(x) / int(y) * 100
+            result = round(result)
+        except (ValueError, ZeroDivisionError): # Execute this statement if the user input is not an integer.
+            pass
+        else:
+            return result
+main()
