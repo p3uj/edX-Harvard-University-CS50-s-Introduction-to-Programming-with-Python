9abb4e037c53f1351594b9e4b79819f4ae64b749
Bengie Villesco <bvillesco@gmail.com>
2023-08-27 16:02:20 +0800

    Modified Problem Set 1/interpreter.py: Removed printing of value x variable and type of the x

--- Problem Set 1/interpreter.py
+++ Problem Set 1/interpreter.py
@@ -1,7 +1,5 @@
 expression = input("Expression: ")
 x, y, z = expression.split(" ") # In the data stored in expression, every "<space>" will separate the data for x, y, x
-print(x)
-print(type(x))
 match y:
     case "+":
         print(f"{float(x) + float(z):.1f}")
